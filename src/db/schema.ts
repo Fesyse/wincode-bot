4783488f1d0c0b063b14e2249dab35e59e9bbaff
fesyse <maksimlol.mail.com@mail.ru>
2024-11-22 18:52:18 +0800

    add types to lesson start and end time

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,6 +3,7 @@ import { relations } from "drizzle-orm"
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core"
 
 const days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"] as const
+type timeType = `${number}:${number}`
 
 export const groups = pgTable("group", {
   id: integer("id").primaryKey().notNull(),
@@ -12,11 +13,18 @@ export const groups = pgTable("group", {
 export const lessons = pgTable("lesson", {
   id: varchar("id", { length: 255 }).primaryKey().$defaultFn(cuid),
 	day: varchar("day", { length: 255, enum: days }).notNull(),
-	startTime: varchar("start_time", { length: 255 }).notNull(),
-	endTime: varchar("end_time", { length: 255 }).notNull(),
+	startTime: varchar("start_time", { length: 255 }).$type<timeType>().notNull(),
+	endTime: varchar("end_time", { length: 255 }).$type<timeType>().notNull(),
   groupId: integer("group_id").references(() => groups.id).notNull(),
 })
 
 export const groupsRelations = relations(groups, ({ many }) => ({
 	lessons: many(lessons),
+}))
+
+export const lessonsRelations = relations(lessons, ({ one }) => ({
+  group: one(groups, {
+    fields: [lessons.groupId],
+    references: [groups.id],
+  }),
 }))
